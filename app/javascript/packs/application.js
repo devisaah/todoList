620891ddd6c148d431dea1fa3cec93688edcff73
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2021-06-18 13:30:52 -0300

    bootstrap/jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import '@popperjs/core'
+import 'bootstrap'
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
